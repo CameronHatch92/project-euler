6ca2c689ce29ab96f89eb5e67e151f98a110f26c
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-15 13:57:35 -0700

    finished pandigital multiple problem

--- src/PandigitalMultiple.java
+++ src/PandigitalMultiple.java
@@ -1,6 +1,22 @@
 public class PandigitalMultiple {
     public static void main(String[] args) {
-        System.out.println(isPan("123456788"));
+        int largest = 918273645;
+        for(int i=92; i<=9876; i++){
+            if(firstNine(i)){
+                String str = givesDigits(i);
+                if(str.length()==9){
+                    int current = Integer.parseInt(str);
+                    System.out.println(current);
+                    if(isPan(str)){
+                        if(current>largest){
+                            largest = current;
+                        }
+                    }
+                }
+
+            }
+        }
+        System.out.println(largest);
     }
 
     public static String givesDigits(int num){
@@ -14,9 +30,6 @@ public class PandigitalMultiple {
     }
 
     public static boolean isPan(String str){
-        if(str.length()!=9){
-            return false;
-        }
         for(int i=1; i<=9; i++){
             String numStr = Integer.toString(i);
             if(!str.contains(numStr)){
@@ -24,5 +37,13 @@ public class PandigitalMultiple {
             }
         }
         return true;
+    }
+
+    public static boolean firstNine(int num){
+        String str = Integer.toString(num);
+        if(str.substring(0,1).equals("9")){
+            return true;
+        }
+        return false;
     }
 }
